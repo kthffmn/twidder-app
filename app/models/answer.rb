545e46d167977123161d9e118ce73fe716063715
David Gabeau <djg400@nyu.edu>
2014-01-17 12:39:46 -0500

    validates answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,4 +2,6 @@ class Answer < ActiveRecord::Base
   attr_accessible :correct, :guess, :tweet_id, :user_id
   belongs_to :user
   belongs_to :tweet
+
+  	validates :guess, :presence => true, :length => { :maximum => 200, :message => "must be between 1 and 200 characters"}
 end
