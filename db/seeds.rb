e261a3dc893e94757cd5f4ed155f792a4daba8fd
Katie Hoffman <ktahoffman@gmail.com>
2014-01-17 16:16:27 -0500

    removes extra migrations

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,54 +6,65 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# users
-user_1 = User.create(name: "Anisha Ramnani",
-												password: "1Anishatest",
-												score: 1,
-												email: "anisha.ramnnani@gmail.com")
-user_2 = User.create(name: "Katie Hoffman",
+User.create(name: "Anisha Ramnani",
+                        # id: 1,
+                        password: "1Anishatest",
+                        score: 1,
+                        email: "anisha.ramnnani@gmail.com")
+
+User.create(name: "Katie Hoffman",
+                        # id: 2,
                         password: "2Katietest",
                         score: 3,
                         email: "ktahoffman@gmail.com")
-user_3 = User.create(name: "Armando Amador",
+User.create(name: "Armando Amador",
+                        # id: 3,
                         password: "3Armandotest",
                         score: 100,
                         email: "arm.amador@gmail.com")
-user_4 = User.create(name: "David Gabeau",
+User.create(name: "David Gabeau",
+                        # id: 4,
                         password: "4Davidtest",
                         score: 999,
                         email: "david.gabeau@gmail.com")
+
+
 # celebrities
-celebrity_1 = Celebrity.create(name: "Justin Beiber",
+Celebrity.create(
+                        # id: 1,
+                        name: "Justin Beiber",
                         handle: "@justinbieber")
-celebrity_2 = Celebrity.create(name: "Lebron James",
+Celebrity.create(
+                        # id: 2,
+                        name: "Lebron James",
                         handle: "@KingJames")
-celebrity_3 = Celebrity.create(name: "Barney Stinson",
+Celebrity.create(
+                        # id: 3,
+                        name: "Barney Stinson",
                         handle: "@itsBroStinson")
 
-# # tweets
-# tweet_1 = Tweet.create(
-#                       celebrity_id: 1,        
-#                       tweet: "live your dream. be creative")
-# tweet_2 = Tweet.create(
-#                       celebrity_id: 2,        
-#                       tweet: "Happy Bday D.wade")
-# tweet_3 = Tweet.create(
-#                       celebrity_id: 3,        
-#                       tweet: "Legiondary.")
+# tweets
+Tweet.create(id: 1,
+                      celebrity_id: 1,        
+                      tweet: "live your dream. be creative")
+Tweet.create(id: 2,
+                      celebrity_id: 2,        
+                      tweet: "Happy Bday D.wade")
+Tweet.create(id: 3,
+                      celebrity_id: 3,        
+                      tweet: "Legiondary.")
 
-# # answers
-# answer_1 = Answer.create(
-#                       celebrity_id: 1,       
-#                       user_id: 2,
-#                       guess: "Live your dream and be creative.")
+# answers
+Answer.create(id: 1,
+                      celebrity_id: 1,       
+                      user_id: 2,
+                      guess: "Live your dream and be creative.")
 
-# answer_2 = Answer.create(
-#                       celebrity_id: 2,       
-#                       user_id: 3,
-#                       guess: "Happy Birthday D.wade")
-# answer_2 = Answer.create(
-#                       celebrity_id: 3,       
-#                       user_id: 3,
-#                       guess: "Legendary.")
-
+Answer.create(id: 2,
+                      celebrity_id: 2,       
+                      user_id: 3,
+                      guess: "Happy Birthday D.wade")
+Answer.create(id: 3,
+                      celebrity_id: 3,       
+                      user_id: 3,
+                      guess: "Legendary.")
