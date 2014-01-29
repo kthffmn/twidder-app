fe0e8c543e68585f5577acdbca04a7f9daf7db53
Armando Amador <Arm.amador@gmail.com>
2014-01-29 17:59:38 -0500

    Create rootpage and links

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Twidder2::Application.routes.draw do
-  get "users/new"
 
   # root  'static_pages#home'
-  match '/signup', to: 'users#new',         via: 'get'
-  match '/home',   to: 'static_pages#home', via: 'get'
-
+  root to: 'static_pages#home'
+  
+  match '/',        to: 'static_pages#home'
+  match '/help',    to: 'static_pages#help'
+  match '/about',   to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
 
   resources :users do
     resources :answers 
