9cd03718b5e3911447e0c9dab16ffaa1d2d86bfc
Armando Amador <Arm.amador@gmail.com>
2014-02-05 10:08:14 -0500

    Fix respond to format error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,14 +5,29 @@ class UsersController < ApplicationController
 
   def index
     @users = User.paginate(page: params[:page])
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @users }
+    end
   end
 
   def show
     @user = User.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @user }
+    end
   end
 
   def new
     @user = User.new
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @user }
+    end
   end
 
   def edit
@@ -21,22 +36,33 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(params[:user])
-    if @user.save
-      sign_in @user
-      flash[:success] = "Welcome to Twidder!"
-    else
-      render 'new'
+
+     respond_to do |format|
+      if @user.save
+        sign_in @user
+        flash[:success] = "Welcome to Twidder!"
+        format.html { redirect_to @user }
+        format.json { render json: @user, status: :created, location: @user }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   def update
     @user = User.find(params[:id])
-    if @user.update_attributes(params[:user])
-      flash[:success] = "Profile updated"
-      sign_in @user
-      redirect_to @user
-    else
-      render 'edit'
+
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        flash[:success] = "Profile updated"
+        sign_in @user
+        format.html { redirect_to @user}
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
     end
   end
 
