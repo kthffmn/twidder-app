6de78938de03a0436348aca124f90fe9ce6f095d
Katie Hoffman <ktahoffman@gmail.com>
2014-03-05 22:52:42 -0500

    adds add_celeb method to celebrity.rb

--- app/models/celebrity.rb
+++ app/models/celebrity.rb
@@ -10,4 +10,17 @@ class Celebrity < ActiveRecord::Base
   def self.add_twidder
     Celebrity.create(:name => 'Twidder', :handle => 'twidder_app')
   end
+
+  def self.add_celebs
+    Celebrity.create(:name => "Lil Scrappy Merlo", :handle => "reallilscrappy")
+    Celebrity.create(:name => "Snoop Lion", :handle => "snooplion")
+    Celebrity.create(:name => "Bruno Mars", :handle => "brunomars")
+    Celebrity.create(:name => "Kendall Jenner", :handle => "KendallJenner")
+    Celebrity.create(:name => "Lisa Raye", :handle => "TheRealLRaye1")
+    Celebrity.create(:name => "Wiz Khalifa", :handle => "wizkhalifa")
+    Celebrity.create(:name => "Ryan Lochte", :handle => "RyanLochte")
+    Celebrity.create(:name => "Shaquille O'Neal", :handle => "SHAQ")
+    Celebrity.create(:name => "Jim Jones", :handle => "jimjonescapo")
+  end
+
 end
